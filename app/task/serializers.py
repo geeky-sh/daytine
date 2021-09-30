1c0a5aa6fc1d0e376805cba41c65d4f55262422a
Aash <aash.discover@gmail.com>
2021-09-30 20:28:24 +0530

    adding example values in pydantic models

--- app/task/serializers.py
+++ app/task/serializers.py
@@ -1,11 +1,11 @@
 import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class TaskBase(BaseModel):
-    date: datetime.date
-    start_time: datetime.time
-    end_time: datetime.time
-    content: str
+    date: datetime.date = Field(example="1991-01-23")
+    start_time: datetime.time = Field(example="7:00")
+    end_time: datetime.time = Field(example="8:00")
+    content: str = Field(max_length=255, example="Exercise")
 
 
 class TaskCreate(TaskBase):
