999bf18c2919c656a24f53c09e185c8d799d41a0
Aash <aash.discover@gmail.com>
2021-10-20 19:43:35 +0530

    adding settings file

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pydantic import BaseModel
+from .settings import SQLALCHEMY_DATABASE_URL
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://aash:@localhost:5432/daytine_dev"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
